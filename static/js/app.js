648b4524d86f76794a8d4dad57d43dcc4f7268d7
Marijose Cavazos <marijosecavazos@iMac.local>
2023-04-18 13:43:13 -0600

    d3 read data

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Read JSON data from url
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    
+    console.log("Here is the JSON data:")
+    console.log(data);
+
+});
